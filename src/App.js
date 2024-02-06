3a205237a876f9438aad4535c06556b630aeb03f
Mark Kotlobay <alfa.phalanx@gmail.com>
2024-02-06 18:54:13 +0200

    main stracture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <Button>Clear</Button>
+      </div>
+      <div className='numFunc'>
+        <Button sign="0"/>
+        <Button sign="1"/>
+        <Button sign="2"/>
+        <Button sign="3"/>
+        <Button sign="4"/>
+        <Button sign="5"/>
+        <Button sign="6"/>
+        <Button sign="7"/>
+        <Button sign="8"/>
+        <Button sign="9"/>
+        <Button sign="."/>
+        <Button sign="="/>
+      </div>
+      <div className='calcFunc'>
+        <Button sign="+" />
+        <Button sign="-" />
+        <Button sign="*" />
+        <Button sign="/" />
+      </div>
+      <div>
+        <Input num="0"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Input(props) {
+  return (
+    <div>
+        <input value={props.num}/>
+    </div>
+  )
+}
+
+export default Input
